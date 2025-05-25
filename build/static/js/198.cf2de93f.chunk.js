fd93432f3c9b147570fababae828a6d365ce3f3a
Maurizio Catalucci <maurizio.mk9@proton.me>
2025-05-24 22:07:13 -0300

    Corrige build e ativa PWA

---
+++ build/static/js/198.cf2de93f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgaleria_app_pwa=self.webpackChunkgaleria_app_pwa||[]).push([[198],{1198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.cf2de93f.chunk.js.map
